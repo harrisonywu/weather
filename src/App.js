a6cfd16497fc916f4917e3944381ebeaf5532312
Stanley Wu <stanleychuwu@gmail.com>
2020-10-22 13:29:43 -0700

    Added form and onChange function

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 function App() {
+  function inputChange (e) {
+    console.log(e.target.value)
+  }
   return (
     <div className="background">
       <div className="card">
         <h1>Hello!</h1>
         <TextField
+          id='city'
           label='City'
           placeholder='Enter a city name'
+          onChange={inputChange}
         >
         </TextField>
         <br />
