2e4b2397521dbe6013b72f6b9db75366f1282ab1
Stanley Wu <stanleychuwu@gmail.com>
2020-10-29 13:58:26 -0700

    Added API call on Enter and Submit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import React from 'react';
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import key from './api/api.js';
+
+// possibly add state to search as well
 
 function App() {
+
+  let city = '';
+  console.log("city = ", city)
+
   function inputChange (e) {
-    console.log(e.target.value)
+    city = e.target.value;
+  }
+
+  function getWeather (str) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then(response => response.json())
+    .then(data => console.log('Success : ', data))
+    .catch(error => console.log('Error : ', error))
+  }
+  function handleEnter (e) {
+    if (e.keyCode === 0) {
+      getWeather(city)
+    }
   }
   return (
     <div className="background">
@@ -16,11 +35,12 @@ function App() {
           label='City'
           placeholder='Enter a city name'
           onChange={inputChange}
+          onKeyPress={handleEnter}
         >
         </TextField>
         <br />
         <br />
-        <Button variant='contained' color='Primary'>
+        <Button variant='contained' color='default' onClick={() => getWeather(city)}>
           Check Weather
         </Button>
       </div> 
